2000a8bd1b0876e2c0ec97363829bcda478edd35
mezhcoder <zhcoder@yandex.ru>
2022-10-20 14:39:07 +1000

    refactoring and added total tests in checker

--- analyzer.js
+++ analyzer.js
@@ -1,5 +1,5 @@
 function analysis(line) {
-    return '1 1 Integer 1 1';
+    return '1 1 Integer 17 0x11';
 }
 
 module.exports = analysis;
--- checker.js
+++ checker.js
@@ -1,12 +1,13 @@
 const fs = require('fs');
 const analysis = require('./analyzer');
 
+
+
 const tests = {}
 for (const nameFile of fs.readdirSync("tests")) {
     const numberTest = nameFile
-        .replace('in', '')
-        .replace('out', '')
-        .replace('.', '')
+        .replace('.in', '')
+        .replace('.out', '')
     if (!tests.hasOwnProperty(numberTest)) {
         tests[numberTest] = {
             'pathFileTestIn': `tests/${numberTest}.in`,
@@ -14,14 +15,16 @@ for (const nameFile of fs.readdirSync("tests")) {
         }
     }
 }
-
+let successTest = 0;
 for (const [key, value] of Object.entries(tests)) {
     const testIn = fs.readFileSync(value.pathFileTestIn, 'utf-8');
     const testOut = fs.readFileSync(value.pathFileTestOut, 'utf-8');
     const result = analysis(testIn);
     if (result === testOut) {
         console.log(`✅ ${key} Test success`);
+        successTest++;
     } else {
         console.log(`❌ ${key} Test error`);
     }
 }
+console.log(`Total tests: ${successTest}/${Object.keys(tests).length}`)
